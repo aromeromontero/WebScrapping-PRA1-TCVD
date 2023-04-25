577c0f407926e082ed86c736f0822f5eaea579d9
diddy98 <92057839+diddy98@users.noreply.github.com>
2023-04-25 17:55:03 +0200

    Update itemsloader.py

--- source/itemsloader.py
+++ source/itemsloader.py
@@ -1,3 +1,5 @@
+# Definición del itemloader, es decir, el mecanismo mediante el cual poblar los ítems. 
+
 from itemloaders.processors import MapCompose
 from scrapy.loader import ItemLoader
 from w3lib.html import replace_escape_chars
@@ -5,7 +7,9 @@ from w3lib.html import replace_escape_chars
 punctuation = '''!-[]{};:'"\,<>./?@#$%^&*_~'''
 
 class SpiderItemLoader(ItemLoader):
+    # Declaración de procesos de input: para título, título original y rating, se reemplazarán los caracteres de escape. 
     title_in = MapCompose(lambda v: v.strip(), replace_escape_chars)
     original_title_in = MapCompose(lambda v: v.strip(), replace_escape_chars)
     rating_in = MapCompose(lambda v: v.strip(), replace_escape_chars)
+    # En production, se eliminarán los signos de puntuación definidos en la línea 7
     production_in = MapCompose(lambda v: v.strip(punctuation))
